8032cadfc135f57e79ae9077ba096d7941d6d761
Shreya Pandey <130491256+shreyapandeeyy@users.noreply.github.com>
2025-10-23 17:08:52 +0530

    Refactor wallpaperHandler for improved clarity

--- components/system/Desktop/Wallpapers/handlers.ts
+++ components/system/Desktop/Wallpapers/handlers.ts
@@ -3,117 +3,117 @@ import {
     type ApodResponse,
     type ArtInstituteOfChicagoResponse,
   } from "components/system/Desktop/Wallpapers/types";
-  import { type WallpaperFit } from "contexts/session/types";
-  import { MILLISECONDS_IN_DAY, MILLISECONDS_IN_MINUTE } from "utils/constants";
-  import {
-    jsonFetch,
-    viewWidth,
-    isYouTubeUrl,
-    getYouTubeUrlId,
-    viewHeight,
-  } from "utils/functions";
-  
-  const API_URL = {
-    APOD: "https://api.nasa.gov/planetary/apod",
-    ART_INSTITUTE_OF_CHICAGO: "https://api.artic.edu/api/v1/artworks/search",
-  };
-  
-  export const wallpaperHandler: Record<string, WallpaperHandler> = {
-    APOD: async (params) => {
-      const { isAlt } = params || {};
-      const response = await jsonFetch(
-        `${API_URL.APOD}?${isAlt ? "count=1&" : ""}api_key=DEMO_KEY`
-      );
-      const { hdurl, url } = (isAlt ? response[0] : response) as ApodResponse;
-  
-      let wallpaperUrl = "";
-      let fallbackBackground = "";
-      let newWallpaperFit = "" as WallpaperFit;
-  
-      if (hdurl || url) {
-        wallpaperUrl = (viewWidth() > 1024 ? hdurl : url) || url || "";
-        newWallpaperFit = "fit";
-  
-        if (isYouTubeUrl(wallpaperUrl)) {
-          const ytBaseUrl = `https://i.ytimg.com/vi/${getYouTubeUrlId(
-            wallpaperUrl
-          )}`;
-  
-          wallpaperUrl = `${ytBaseUrl}/maxresdefault.jpg`;
-          fallbackBackground = `${ytBaseUrl}/hqdefault.jpg`;
-        } else if (hdurl && url && hdurl !== url) {
-          fallbackBackground = wallpaperUrl === url ? hdurl : url;
-        }
-      }
-  
-      return {
-        fallbackBackground,
-        newWallpaperFit,
-        updateTimeout: MILLISECONDS_IN_DAY,
-        wallpaperUrl,
-      };
-    },
-    ART_INSTITUTE_OF_CHICAGO: async () => {
-      const requestPayload = {
-        boost: false,
-        fields: ["image_id"],
-        limit: 1,
+import { type WallpaperFit } from "contexts/session/types";
+import { MILLISECONDS_IN_DAY, MILLISECONDS_IN_MINUTE } from "utils/constants";
+import {
+jsonFetch,
+viewWidth,
+isYouTubeUrl,
+getYouTubeUrlId,
+viewHeight,
+} from "utils/functions";
+
+const API_URL = {
+APOD: "https://api.nasa.gov/planetary/apod",
+ART_INSTITUTE_OF_CHICAGO: "https://api.artic.edu/api/v1/artworks/search",
+};
+
+export const wallpaperHandler: Record<string, WallpaperHandler> = {
+APOD: async (params) => {
+    const { isAlt } = params || {};
+    const response = await jsonFetch(
+    `${API_URL.APOD}?${isAlt ? "count=1&" : ""}api_key=DEMO_KEY`
+    );
+    const { hdurl, url } = (isAlt ? response[0] : response) as ApodResponse;
+
+    let wallpaperUrl = "";
+    let fallbackBackground = "";
+    let newWallpaperFit = "" as WallpaperFit;
+
+    if (hdurl || url) {
+    wallpaperUrl = (viewWidth() > 1024 ? hdurl : url) || url || "";
+    newWallpaperFit = "fit";
+
+    if (isYouTubeUrl(wallpaperUrl)) {
+        const ytBaseUrl = `https://i.ytimg.com/vi/${getYouTubeUrlId(
+        wallpaperUrl
+        )}`;
+
+        wallpaperUrl = `${ytBaseUrl}/maxresdefault.jpg`;
+        fallbackBackground = `${ytBaseUrl}/hqdefault.jpg`;
+    } else if (hdurl && url && hdurl !== url) {
+        fallbackBackground = wallpaperUrl === url ? hdurl : url;
+    }
+    }
+
+    return {
+    fallbackBackground,
+    newWallpaperFit,
+    updateTimeout: MILLISECONDS_IN_DAY,
+    wallpaperUrl,
+    };
+},
+ART_INSTITUTE_OF_CHICAGO: async () => {
+    const requestPayload = {
+    boost: false,
+    fields: ["image_id"],
+    limit: 1,
+    query: {
+        function_score: {
+        boost_mode: "replace",
         query: {
-          function_score: {
-            boost_mode: "replace",
-            query: {
-              bool: {
-                filter: [
-                  {
-                    term: {
-                      is_public_domain: true,
-                    },
-                  },
-                  {
-                    terms: {
-                      artwork_type_id: [1], // Painting
-                    },
-                  },
-                  {
-                    exists: {
-                      field: "image_id",
-                    },
-                  },
-                ],
-              },
+            bool: {
+            filter: [
+                {
+                term: {
+                    is_public_domain: true,
+                },
+                },
+                {
+                terms: {
+                    artwork_type_id: [1], // Painting
+                },
+                },
+                {
+                exists: {
+                    field: "image_id",
+                },
+                },
+            ],
             },
-            random_score: {
-              field: "id",
-              seed: Date.now(),
-            },
-          },
         },
-      };
-      const response = (await fetch(API_URL.ART_INSTITUTE_OF_CHICAGO, {
-        body: JSON.stringify(requestPayload),
-        headers: {
-          "Content-Type": "application/json",
+        random_score: {
+            field: "id",
+            seed: Date.now(),
         },
-        method: "POST",
-      }).then(res => res.json())) as ArtInstituteOfChicagoResponse;
-      const imageUrl = (isMaxSize: boolean): string =>
-        response?.data?.[0]?.image_id
-          ? `https://www.artic.edu/iiif/2/${response.data[0].image_id}/full/${
-              isMaxSize ? "1686" : "843"
-            },/0/default.jpg`
-          : "";
-  
-      return {
-        fallbackBackground: imageUrl(false),
-        newWallpaperFit: "fit",
-        updateTimeout: MILLISECONDS_IN_MINUTE * 60,
-        wallpaperUrl: imageUrl(true),
-      };
+        },
     },
-    LOREM_PICSUM: () => ({
-      fallbackBackground: "",
-      newWallpaperFit: "fill",
-      updateTimeout: MILLISECONDS_IN_MINUTE * 60,
-      wallpaperUrl: `https://picsum.photos/seed/${Date.now()}/${viewWidth()}/${viewHeight()}`,
-    }),
-  };
+    };
+    const response = (await fetch(API_URL.ART_INSTITUTE_OF_CHICAGO, {
+    body: JSON.stringify(requestPayload),
+    headers: {
+        "Content-Type": "application/json",
+    },
+    method: "POST",
+    }).then(res => res.json())) as ArtInstituteOfChicagoResponse;
+    const imageUrl = (isMaxSize: boolean): string =>
+    response?.data?.[0]?.image_id
+        ? `https://www.artic.edu/iiif/2/${response.data[0].image_id}/full/${
+            isMaxSize ? "1686" : "843"
+        },/0/default.jpg`
+        : "";
+
+    return {
+    fallbackBackground: imageUrl(false),
+    newWallpaperFit: "fit",
+    updateTimeout: MILLISECONDS_IN_MINUTE * 60,
+    wallpaperUrl: imageUrl(true),
+    };
+},
+LOREM_PICSUM: () => ({
+    fallbackBackground: "",
+    newWallpaperFit: "fill",
+    updateTimeout: MILLISECONDS_IN_MINUTE * 60,
+    wallpaperUrl: `https://picsum.photos/seed/${Date.now()}/${viewWidth()}/${viewHeight()}`,
+}),
+};
